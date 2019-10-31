1fb56f6c9a5476c3ae20830def04de92a85a107f
Konstantin <konstantin.v.modin@gmail.com>
2019-10-31 06:43:07 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 const App = () => 
     <div className="App">
-        <p>1. Turn this app into one which does NOT use local state (in components) but instead uses Redux</p>        
+        <p>Example of Redux-based state</p>        
         <hr />
         <Persons />
     </div>; 
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -9,10 +9,12 @@ const reducer = (state = initialState, { type, payload }) => {
         case 'ADD': {
             console.log(state, type, payload);
             const personName = payload.name ? 
-                payload.name : ['Bob', 'Dan', 'Peter', 'Max', 'Ray', 'Rob'][~~(Math.random() * 10) % 6];
+                payload.name : 
+                ['Bob', 'Dan', 'Peter', 'Max', 'Ray', 'Rob'][~~(Math.random() * 10) % 6];
             
                 const personAge = payload.name ? 
-                payload.age : ~~(Math.random() * 50);
+                    payload.age : 
+                    ~~(Math.random() * 50);
 
             const person = {
                 id: Math.random() * Math.random(),
@@ -28,9 +30,7 @@ const reducer = (state = initialState, { type, payload }) => {
         case 'DEL':
             return {
                 ...state,
-                persons: state.persons.filter(({
-                    id
-                }) => id !== payload)
+                persons: state.persons.filter(({id}) => id !== payload)
             };
         default:
             return state
